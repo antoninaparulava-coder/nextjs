0e4f696838ff63f125414828562e1f72f85dd7f9
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-07-02 18:14:05 +0400

    Working With Fallback Pages

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
+import fs from 'fs/promises'
+import Link from 'next/link';
 import path from 'path';
-import fs from 'fs/promises';
 
 function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
-      {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+      {products.map(product => (
+      <li key={product.id}>
+      <Link href={`/${product.id}`}>{product.title}</Link>
+      </li>
       ))}
     </ul>
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
