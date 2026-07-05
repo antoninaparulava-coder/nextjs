c8da5d0167de2da778ae9027ffd58730eca81a1a
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-07-05 18:18:34 +0400

    Using the useSWR NextJS Hook

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,10 +1,31 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalesPage() {
     const [sales ,setSales] = useState();
-    const [isLoading, setIsLoading] = useState(false);
+    //const [isLoading, setIsLoading] = useState(false);
+
+    const fetcher = (url) => fetch(url).then((res)  => res.json())
+
+    const { data, error } = useSWR('https://nextjs-course-d3acb-default-rtdb.firebaseio.com/sales.json',fetcher);
 
     useEffect(() => {
+        if(data){
+            const transformedSales = [];
+
+            for ( const key in data) {
+                transformedSales.push({
+                    id: key, 
+                    username: data[key].username, 
+                    volume: data[key].volume
+                })
+            }
+
+            setSales(transformedSales);
+        }
+    }, [data])
+
+    /* useEffect(() => {
         setIsLoading(true);
         fetch(
             'https://nextjs-course-d3acb-default-rtdb.firebaseio.com/sales.json'
@@ -24,14 +45,14 @@ function LastSalesPage() {
             setSales(transformedSales)
             setIsLoading(false)
         });
-    }, []) ;
-    
-    if(isLoading){
-        return <p>Loading...</p>
-    }
+    }, []) ; */
 
-    if(!sales){
-        return <p>No data yet</p>
+    if(error){
+        return <p>Failed to load.</p>
+    }
+    
+    if(!data || !sales){
+        return <p>Loading...</p>
     }
 
     return <ul>
